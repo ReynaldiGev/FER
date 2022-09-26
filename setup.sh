e358b2e8270fa5ee9a7b77fa0dd06ef32b2bbb3e
ReynaldiGev <reynaldihutapea15@gmail.com>
2022-09-26 15:05:26 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,9 @@
-mkdir -p ~/.streamlit/
+mkdir -p ~ / .streamlit /
 
-echo "\
-[general]\n\
-email = \"reynaldihutapea15@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
+echo " \
+[ server ] \ n \
+headless = true \ n \
+port = $ PORT \ n \
+enableCORS = false \ n \
+\ n \
+" > ~ / .streamlit / config.toml
