54ff8a294ad0693ba4e50dfc444eb2431b475962
ReynaldiGev <reynaldihutapea15@gmail.com>
2022-09-29 10:59:36 +0700

    update

--- app.py
+++ app.py
@@ -19,8 +19,6 @@ import plotly.express as px
 import plotly.graph_objects as go
 import numpy as np
 import pandas as pd
-import seaborn as sns
-import glob as gb
 import os
 from PIL import Image
 import tensorflow as tf
